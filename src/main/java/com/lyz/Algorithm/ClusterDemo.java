d101580713ed071f728d553d3a3e356621d64c76
lyzfly <1084259255@qq.com>
2019-05-29 01:21:58 +0800

    java driver operation

---
+++ src/main/java/com/lyz/Algorithm/ClusterDemo.java
@@ -0,0 +1,28 @@
+package com.lyz.Algorithm;
+
+import org.neo4j.driver.v1.*;
+
+public class ClusterDemo {
+
+    public static void main(String[] args) {
+
+        Driver driver = GraphDatabase.driver("bolt://localhost:7687",AuthTokens.basic("neo4j", "0228"));
+        try(Session session = driver.session()){
+            try(Transaction tx = session.beginTransaction()){
+                StatementResult result = tx.run("CALL algo.degree.stream(\"User\", \"FOLLOWS\", {direction: \"incoming\"})\n" +
+                        "YIELD nodeId, score\n" +
+                        "RETURN algo.asNode(nodeId).id AS name, score AS followers\n" +
+                        "ORDER BY followers DESC");
+                while(result.hasNext()){
+                    Record record = result.next();
+
+                    System.out.println(record.asMap());
+                    System.out.println(record.get("name"));
+                    System.out.println(record.get("followers"));
+                }
+
+                tx.success();
+            }
+        }
+    }
+}
